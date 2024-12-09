f3372a8182d2a9163207bd535de75b4f447ad9d0
jamjamstone <junghyun4545@naver.com>
2024-12-09 09:38:52 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace day13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ///revert => 역순으로 기록하는 방법 1->2->3->4->4-1->3-1
+            ///reset => 1->2->3->4-> 3번과 4번 삭제 => 1->2->3-1->4-1
+            ///컨트롤 쉬프트 f => 저장공간 불러오기
+            //File.Open("C:\\Users\\KGA\\Desktop\\testing\\ConsoleApp1",FileMode.Open);
+
+
+
+        }
+    }
+}
